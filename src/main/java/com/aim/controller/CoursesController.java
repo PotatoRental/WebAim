5dcfb80d6de4733aaf9e27b1a7cc131649672660
Jason Chen <milkphany@gmail.com>
2013-11-25 13:36:48 -0500

    Fixed course and survey

--- src/main/java/com/aim/controller/CoursesController.java
+++ src/main/java/com/aim/controller/CoursesController.java
@@ -1,7 +1,8 @@
 package com.aim.controller;
 
 import com.aim.model.Course;
-import com.aim.service.CourseServiceImpl;
+import com.aim.model.UserAccount;
+import com.aim.service.AimServiceImpl;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -10,7 +11,6 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.ArrayList;
 import java.util.List;
